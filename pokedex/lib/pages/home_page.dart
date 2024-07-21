b66b1388c1f897a0ff0cc1da2afe1088a25ed3ab
Rithik1611 <rithik161104@gmail.com>
2024-07-21 19:40:39 +0530

    notifierprovider setup

---
+++ pokedex/lib/pages/home_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/controller/home_page_controller.dart';
+import 'package:pokedex/models/page_data.dart';
+
+final homePageControllerProvider =
+    StateNotifierProvider<HomePageController, HomePageData>(
+  (ref) {
+    return HomePageController(HomePageData.initial());
+  },
+);
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  late HomePageController _homePageController;
+  late HomePageData _homePageData;
+
+  @override
+  Widget build(BuildContext context) {
+    _homePageController = ref.watch(homePageControllerProvider.notifier);
+    _homePageData = ref.watch(homePageControllerProvider);
+    return _buildUI(context);
+  }
+
+  Widget _buildUI(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.02,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [_allPokemon(context)],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _allPokemon(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "All Pokemons",
+            style: TextStyle(fontSize: 25),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.60,
+            child: ListView.builder(
+              itemCount: 0,
+              itemBuilder: (context, index) {
+                return const ListTile();
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
