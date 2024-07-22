aafee847a388b6a0bbef59c334a8e9d09e391d44
Rithik1611 <rithik161104@gmail.com>
2024-07-22 19:42:51 +0530

    skeletonizer

--- pokedex/lib/controller/home_page_controller.dart
+++ pokedex/lib/controller/home_page_controller.dart
@@ -1,6 +1,31 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:pokedex/models/page_data.dart';
+import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/services/https_service.dart';
 
 class HomePageController extends StateNotifier<HomePageData> {
-  HomePageController(super._state);
+  final GetIt _getIt = GetIt.instance;
+  late HttpsService _httpsService;
+
+  HomePageController(super._state) {
+    _httpsService = _getIt.get<HttpsService>();
+    setup();
+  }
+  Future<void> setup() async {
+    loaddata();
+  }
+
+  Future<void> loaddata() async {
+    if (state.data == null) {
+      Response? res = await _httpsService
+          .get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
+      if (res != null && res.data != null) {
+        PokemonListData data = PokemonListData.fromJson(res.data);
+        state = state.copyWith(data: data);
+        print(state.data?.results?.first);
+      }
+    } else {}
+  }
 }
---
+++ pokedex/lib/widget/pokemon_list_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/provider/pokemon_data_provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class PokemonListTile extends ConsumerWidget {
+  const PokemonListTile({super.key, required this.pokemonurl});
+
+  final String pokemonurl;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemon = ref.watch(pokemonDataProvider(pokemonurl));
+    return pokemon.when(data: (data) {
+      return _tile(context, false, data);
+    }, error: (error, stackTrace) {
+      return Text("Error: $error");
+    }, loading: () {
+      return _tile(context, true, null);
+    });
+  }
+
+  Widget _tile(BuildContext context, bool isLoading, Pokemon? pokemon) {
+    return Skeletonizer(
+      enabled: isLoading,
+      child: ListTile(
+        title: Text(pokemon != null
+            ? pokemon.name!.toUpperCase()
+            : "Currently loading your Pokemon"),
+        subtitle: Text("Has ${pokemon?.moves?.length.toString() ?? 0} moves"),
+      ),
+    );
+  }
+}
